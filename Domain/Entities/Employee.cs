5ea26d3b7ec1b0f23a7c045bd895cb65bf2186bc
carlos richter <rrch.cr8@gmail.com>
2021-11-16 13:25:44 -0400

    entity added

---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string JobTitle { get; set; }
+        public string Phone { get; set; }
+        public string ImageUrl { get; set; }
+        public int EmployeeCode { get; set; }
+
+
+
+
+
+    }
+}
